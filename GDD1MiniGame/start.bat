b7377cbd6587f954cfff513391977d25a3ed41a0
Nebelwaffel <69768692+Nebelwaffel@users.noreply.github.com>
2022-10-28 14:49:52 +0200

    Delete GDD1MiniGame directory

--- GDD1MiniGame/start.bat
+++
@@ -1,4 +0,0 @@
-SET Browser="C:\Program Files\Google\Chrome\Application\chrome.exe"
-%Browser% "%cd%\index.html" --allow-file-access-from-files
-start cmd.exe /c python -m http.server
-%Browser% "http://localhost:8000/"
